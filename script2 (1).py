7f9a41630d24438e590d1cac2870ea2522d2533a
CMouparna <110818442+CMouparna@users.noreply.github.com>
2022-08-17 12:48:54 +0530

    Add files via upload

---
+++ script2 (1).py
@@ -0,0 +1,12 @@
+num1=int(input('Enter first number:'))
+num2=int(input('Enter second number:'))
+if num1<=num2:
+if num1==num2:
+print('Both numbers are equal.')
+else:
+print('second number is greater')
+
+
+
+
+
