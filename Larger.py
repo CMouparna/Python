2733ec169eeff42ed7c2c5fda36d1a035feec4c5
CMouparna <110818442+CMouparna@users.noreply.github.com>
2022-08-17 12:24:28 +0530

    Add files via upload

---
+++ Larger.py
@@ -0,0 +1,4 @@
+if n1<n2:
+print('The Number',n1,'is smaller than',n2)
+else:
+print('The number',n2,'is smaller than',n1)
---
+++ smaller.py
@@ -0,0 +1,4 @@
+if n1>n2:
+print('The Number',n1,'is greater than',n2)
+else:
+print('The Number',n2,'is greater than',n2)
